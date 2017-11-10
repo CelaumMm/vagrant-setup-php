4d0c0a7ccd3dafbd8d457d9a4dde09fedd63592e
Marcelo Camargo <celaummm@gmail.com>
2017-11-10 15:26:57 -0200

    Alterado o SHELL SCRIPT

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,6 @@ Vagrant.configure("2") do |config|
     config.vm.network :forwarded_port, guest: 80, host: 8080
     config.vm.network :private_network, ip: "192.168.33.10"
     config.vm.synced_folder "www/", "/var/www", owner: "www-data", group: "www-data", mount_options: ['dmode=777','fmode=666']
-    #config.vm.synced_folder "~", "/vagrant", owner: "vagrant", group: "vagrant"
     config.vm.synced_folder "./", "/vagrant", owner: "vagrant", group: "vagrant"
     config.vm.provider "virtualbox" do |machine|
     	machine.memory = 1024
--- setup.sh
+++ setup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 clear
+
 echo "----------------------------------------------------------------"
 echo "---- INICIANDO INSTALACAO DO AMBIENTE DE DESENVOLVIMENTO PHP ---"
 echo "----------------------------------------------------------------"
 echo ""
 
+echo "------------------------------------------------------------------"
+echo "--- Adicionando repositorio atual do pacote PHP, APACHE, MYSQL ---"
+echo "------------------------------------------------------------------"
+sudo apt-get install python-software-properties -y
+
+sudo add-apt-repository ppa:ondrej/php
+#sudo add-apt-repository ppa:ondrej/apache2
+sudo add-apt-repository ppa:ondrej/mysql-5.7
+sudo add-apt-repository ppa:nijel/phpmyadmin
+echo ""
+
 echo "------------------------------------"
 echo "--- Atualizando lista de pacotes ---"
 echo "------------------------------------"
-sudo apt-get update
+sudo apt-get update && sudo apt-get -y upgrade
 echo ""
+
+clear
 
 echo "--------------------------------------------------------------"
 echo "--- Definindo Senha padrao para o MySQL e suas ferramentas ---"
@@ -33,37 +47,50 @@ phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
 EOF
 echo ""
 
+clear
+
 echo "----------------------------------"
 echo "--- Instalando pacotes basicos ---"
 echo "----------------------------------"
-sudo apt-get install vim curl python-software-properties git-core software-properties-common --assume-yes --force-yes
+sudo apt-get install vim curl git-core software-properties-common --assume-yes --force-yes
 echo ""
 
-echo "---------------------------------------------"
-echo "--- Adicionando repositorio do pacote PHP ---"
-echo "---------------------------------------------"
-sudo add-apt-repository ppa:ondrej/php
-echo ""
-
-echo "------------------------------------"
-echo "--- Atualizando lista de pacotes ---"
-echo "------------------------------------"
-sudo apt-get update
-echo ""
+clear
 
 echo "------------------------------------------------------------"
 echo "--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
 echo "------------------------------------------------------------"
 sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
 echo ""
-
-echo "-----------------------------------------------"
-echo "--- Instalando PHP, Apache e alguns modulos ---"
-echo "-----------------------------------------------"
-sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
-sudo apt-get install php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring zip unzip --assume-yes --force-yes
+echo "Copiando as configurações do phpmyadmin"
+echo "---------------------------------------"
+sudo cp -r -f /vagrant/config/etc/phpmyadmin/. /etc/phpmyadmin/.
 echo ""
 
+clear
+
+echo "----------------------"
+echo "--- Instalando PHP ---"
+echo "----------------------"
+sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
+
+echo "--------------------------"
+echo "--- Instalando modulos ---"
+echo "--------------------------"
+sudo apt-get install php7.1-cli --assume-yes --force-yes
+sudo apt-get install libapache2-mod-php7.1 --assume-yes --force-yes
+sudo apt-get install php7.1-mysql --assume-yes --force-yes
+sudo apt-get install php7.1-curl --assume-yes --force-yes
+sudo apt-get install php-memcached --assume-yes --force-yes
+sudo apt-get install php7.1-dev --assume-yes --force-yes
+sudo apt-get install php7.1-mcrypt --assume-yes --force-yes
+sudo apt-get install php7.1-sqlite3 --assume-yes --force-yes
+sudo apt-get install php7.1-mbstring --assume-yes --force-yes
+sudo apt-get install zip --assume-yes --force-yes
+sudo apt-get install unzip --assume-yes --force-yes
+echo ""
+
+clear
 
 echo "-----------------------------"
 echo "--- Habilitando o PHP 7.1 ---"
